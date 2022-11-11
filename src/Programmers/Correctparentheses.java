0c5a19db36bd16bd1654082665c53289f6ad263d
hkfoodis <dlaehdud96@naver.com>
2022-11-11 23:20:36 +0900

    올바른 괄호

---
+++ src/Programmers/Correctparentheses.java
@@ -0,0 +1,34 @@
+package Programmers;
+
+import java.util.Stack;
+
+public class Correctparentheses {
+
+    static boolean solution(String s) {
+        boolean answer = true;
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c == '(') {
+                stack.push(c);
+            } else {
+                if (stack.size() == 0) {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+
+        return stack.size() == 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("(())()"));
+    }
+
+
+}
